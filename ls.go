6775e12c55ed918ebb966d574294559098966485
Viswanath KSP <kspviswa.github@gmail.com>
2017-10-21 19:26:16 +0530

    Included CLI framework

--- ls.go
+++ ls.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/urfave/cli"
 )
 
 func checkerr(err error) {
@@ -22,9 +24,37 @@ func serveDir(dir string) {
 }
 
 func main() {
-	if len(os.Args) > 1 {
-		serveDir(os.Args[1])
-	} else {
-		serveDir(".")
+	app := cli.NewApp()
+	app.Name = "ls ( go implemenetation )"
+	app.Usage = "ls <flags> <args>"
+	app.Author = "Viswanath Kumar Skand Priya"
+	app.Email = "kspviswa.github@gmail.com"
+	app.Version = "0.1"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{Name: "l, long"},
+		cli.BoolFlag{Name: "d, dronly"},
 	}
+	app.Commands = []cli.Command{
+		{
+			Name:    "tree",
+			Aliases: []string{"tr"},
+			Usage:   "perform recursive directory lookup",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("long") {
+			fmt.Println("Long value in flag")
+		}
+		if c.NArg() > 0 {
+			serveDir(c.Args()[0])
+		} else {
+			serveDir(".")
+		}
+		return nil
+	}
+
+	app.Run(os.Args)
 }
