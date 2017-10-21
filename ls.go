bc3d2b5d16e28a42e9fdcbe4fd4c6aa97119c6d0
Viswanath Kumar Skand Priya <kspviswa.github@gmail.com>
2017-10-21 21:03:00 +0530

    Added support for -f, -d, -hr & time format

--- ls.go
+++ ls.go
@@ -3,15 +3,60 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	//"strconv"
 	"time"
 
+	"code.cloudfoundry.org/bytefmt"
+	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
 )
+
+type flagdef struct {
+	isLong   bool
+	isDronly bool
+	isFOnly  bool
+	isHr     bool
+}
 
 func checkerr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func retsize(nsize int64, hr bool) string {
+	if hr {
+		return bytefmt.ByteSize(uint64(nsize))
+	}
+	return fmt.Sprintf("%v", nsize)
+}
+
+func serveDecoratedDir(dir string, flags flagdef) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Size", "Perms", "At", "Dir"})
+
+	f, err := os.OpenFile(dir, os.O_RDONLY, 0666)
+	checkerr(err)
+	finfo, err := f.Readdir(0)
+	checkerr(err)
+	for _, item := range finfo {
+		if flags.isDronly && item.Mode().IsRegular() {
+			continue
+		}
+		if flags.isFOnly && item.IsDir() {
+			continue
+		}
+		data := []string{
+			item.Name(),
+			retsize(item.Size(), flags.isHr),
+			item.Mode().String(),
+			item.ModTime().Format("2006-01-02 15:04:05"),
+			strconv.FormatBool(item.IsDir()),
+		}
+		table.Append(data)
+	}
+	table.Render()
 }
 
 func serveDir(dir string) {
@@ -42,6 +87,14 @@ func main() {
 			Name:  "d, dronly",
 			Usage: "include only directories",
 		},
+		cli.BoolFlag{
+			Name:  "f, fileonly",
+			Usage: "include only regular files",
+		},
+		cli.BoolFlag{
+			Name:  "hr",
+			Usage: "view size in humar readable format",
+		},
 	}
 	app.Commands = []cli.Command{
 		{
@@ -54,13 +107,16 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) error {
-		if c.Bool("long") {
-			fmt.Println("Long value in flag")
-		}
+		decor := c.Bool("long") || c.Bool("dronly") || c.Bool("fileonly") || c.Bool("hr")
+		flags := flagdef{c.Bool("long"), c.Bool("dronly"), c.Bool("fileonly"), c.Bool("hr")}
+		dir := "."
 		if c.NArg() > 0 {
-			serveDir(c.Args()[0])
+			dir = c.Args()[0]
+		}
+		if decor {
+			serveDecoratedDir(dir, flags)
 		} else {
-			serveDir(".")
+			serveDir(dir)
 		}
 		return nil
 	}
